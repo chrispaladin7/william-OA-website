a15eb69adb5cc447d75ad5377ef9628a6b664649
chrispaladin7 <chrismartey@yahoo.com>
2023-11-21 13:54:05 -0500

    Adds homepage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,28 +2,24 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
+import HomePage from "../../pages/Homepage"
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
 
   return (
+  <>
+    <header>
+      <NavBar user={user} setUser={setUser} />
+    </header>
     <main className="App">
-      { user ?
-          <>
-            <NavBar user={user} setUser={setUser} />
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
-            </Routes>
-          </>
-          :
-          <AuthPage setUser={setUser} />
-      }
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<AuthPage setUser={setUser} />} />
+      </Routes>
     </main>
+    </>
   );
 }
