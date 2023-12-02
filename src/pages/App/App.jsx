e1313bf58b344731a2c87abf3febde0b14d6e9cc
chrispaladin7 <chrismartey@yahoo.com>
2023-12-02 01:04:26 -0600

    Fixes navbar error

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
-import HomePage from "../../pages/Homepage"
+import HomePage from "../../pages/Homepage/Homepage"
 import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
 
